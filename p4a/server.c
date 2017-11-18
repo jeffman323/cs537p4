be961d09c3ed6dece92e471b348e1262cf041048
jeffman323 <jetucker@wisc.edu>
2017-11-18 10:05:05 -0600

    Added base code for xv6 and non-xv6
    With a few small updates to non-xv6

---
+++ p4a/server.c
@@ -0,0 +1,68 @@
+#include "cs537.h"
+#include "request.h"
+
+// 
+// server.c: A very, very simple web server
+//
+// To run:
+//  server <portnum (above 2000)>
+//
+// Repeatedly handles HTTP requests sent to this port number.
+// Most of the work is done within routines written in request.c
+//
+
+// CS537: Parse the new arguments too
+void getargs(int *port, int *threads, int *buffers, int argc, char *argv[])
+{
+    if (argc != 4) {
+	fprintf(stderr, "Usage: %s <port> <threads> <buffers>\n", argv[0]);
+	exit(1);
+    }
+    *port = atoi(argv[1]);
+    *threads = atoi(argv[2]);
+    if(*threads <= 0) {
+      fprintf(stderr, "Threads must be a positive integer\n");
+      exit(1);  
+    }
+    *buffers = atoi(argv[3]);
+    if(*buffers <= 0) {
+      fprintf(stderr, "Buffers must be a positive integer\n");
+      exit(1);
+    }
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    int listenfd, connfd, port, clientlen, threads, buffers;
+    struct sockaddr_in clientaddr;
+
+    getargs(&port, &threads, &buffers, argc, argv);
+
+    // 
+    // CS537: Create some threads...
+    //
+
+    listenfd = Open_listenfd(port);
+    while (1) {
+	clientlen = sizeof(clientaddr);
+	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+
+	// 
+	// CS537: In general, don't handle the request in the main thread.
+	// Save the relevant info in a buffer and have one of the worker threads 
+	// do the work. 
+	// 
+	requestHandle(connfd);
+
+	Close(connfd);
+    }
+
+}
+
+
+    
+
+
+ 
